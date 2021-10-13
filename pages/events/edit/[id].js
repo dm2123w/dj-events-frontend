f33c4e187e92c31216a7fb579f6efc40ef4d1b20
dmitriysemenko <dm123semenko@gmail.com>
2021-10-13 19:18:18 +0500

    update

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import Image from "next/image";
 import Layout from "@/components/Layout";
+import Modal from "@/components/Modal";
 import { API_URL } from "@/config/index";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -23,6 +24,7 @@ export default function EditEventPage({ evt }) {
     const [imagePreview, setImagePreview] = useState(
         evt.image ? evt.image.formats.thumbnail.url : null
     );
+    const [showModal, setShowModal] = useState(false);
 
     const router = useRouter();
 
@@ -147,10 +149,17 @@ export default function EditEventPage({ evt }) {
                 </div>
             )}
             <div>
-                <button className="btn-secondary">
+                <button
+                    onClick={() => setShowModal(true)}
+                    className="btn-secondary"
+                >
                     <FaImage /> Set Image
                 </button>
             </div>
+
+            <Modal show={showModal} onClose={() => setShowModal(false)}>
+                Image Upload
+            </Modal>
         </Layout>
     );
 }
