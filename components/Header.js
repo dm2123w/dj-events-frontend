b6f7153082c401d53f1c026fa47c23bd4045dcd4
dmitriysemenko <dm123semenko@gmail.com>
2021-10-13 22:14:52 +0500

    update

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import styles from "@/styles/Header.module.css";
 import Search from "./Search";
 
@@ -21,6 +22,11 @@ export default function Header() {
                     <li>
                         <Link href="/events/add">
                             <a>Add Event</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/account/login">
+                            <a className="btn-secondary btn-icon"><FaSignInAlt/>Login</a>
                         </Link>
                     </li>
                 </ul>
