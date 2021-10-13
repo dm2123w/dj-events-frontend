35d8b1ad9044a6ad72e62b55fdbb9fff712d5bf0
dmitriysemenko <dm123semenko@gmail.com>
2021-10-13 23:38:56 +0500

    update

---
+++ context/AuthContext.js
@@ -0,0 +1,38 @@
+import { useState, useEffect, createContext } from "react";
+import { useRouter } from "next/router";
+import { API_URL } from "../config";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [error, setError] = useState(null);
+
+    // Register user
+    const register = async (user) => {
+        console.log(user);
+    };
+
+    // Login user
+    const login = async ({ email: identifier, password }) => {
+        console.log({ identifier, password });
+    };
+
+    // Logout user
+    const logout = async () => {
+        console.log("Logout");
+    };
+
+    // Check if user is logged in
+    const checkUserLoggedIn = async (user) => {
+        console.log("Check");
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, error, register, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
--- pages/account/login.js
+++ pages/account/login.js
@@ -4,13 +4,18 @@ import { FaUser } from "react-icons/fa";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Layout from "@/components/Layout";
+import AuthContext from "@/context/AuthContext";
 import styles from "@/styles/AuthForm.module.css";
 
 export default function LoginPage() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    const { login, error } = useContext(AuthContext);
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        login({ email, password });
     };
 
     return (
@@ -25,6 +30,7 @@ export default function LoginPage() {
                         <label htmlFor="email">Email Address</label>
                         <input
                             type="email"
+                            id="email"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                         />
@@ -33,6 +39,7 @@ export default function LoginPage() {
                         <label htmlFor="password">Password</label>
                         <input
                             type="password"
+                            id="password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                         />
