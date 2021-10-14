0e800998426b9c7e4d8a94dd54aac5b3616e9e47
dmitriysemenko <dm123semenko@gmail.com>
2021-10-14 19:56:17 +0500

    update

--- components/ImageUpload.js
+++ components/ImageUpload.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { API_URL } from "../config/";
+import { API_URL } from "@/config/index";
 import styles from "@/styles/Form.module.css";
 
 export default function ImageUpload({ evtId, imageUploaded }) {
--- pages/api/login.js
+++ pages/api/login.js
@@ -3,8 +3,27 @@ import { API_URL } from "@/config/index";
 export default async (req, res) => {
     if (req.method === "POST") {
         const { identifier, password } = req.body;
-        console.log(req.body);
-        res.status(200).json({});
+
+        const strapiRes = await fetch(`${API_URL}/auth/local`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                identifier,
+                password,
+            }),
+        });
+
+        const data = await strapiRes.json();
+
+        if (strapiRes.ok) {
+            res.status(200).json({ user: data.user });
+        } else {
+            res.status(data.statusCode).json({
+                message: data.message[0].messages[0].message,
+            });
+        }
     } else {
         res.setHeader("Allow", ["POST"]);
         res.status(405).json({ message: `Method ${req.method} not allowed` });
